bcd82bf820c0184b82fdb2b8b669a9a61f959c8a
xiaoyu <xiaoyu@sulg.top>
2021-12-29 22:44:52 +0800

    🐛 修复create-react-app中替换异常的bug

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,40 @@ const fileToBase64 = (filePath) => {
   return new Buffer(data).toString("base64")
 }
 
+const getAssets = (compilation) => {
+  const entrypoints = compilation.entrypoints;
+  const entryNames = Array.from(entrypoints.keys());
+  let files = []
+  for (let i = 0; i < entryNames.length; i++) {
+    const entryName = entryNames[i];
+    const entryFiles = entrypoints.get(entryName).getFiles();
+    files.push(...entryFiles)
+  }
+  function unique(arr) {
+    return arr.filter(function (item, index, arr) {
+      return arr.indexOf(item, 0) === index;
+    });
+  }
+
+  files = unique(files)
+  const assets = {
+    js: [],
+    css: [],
+    uncase: [] // 意料之外的文件
+  }
+
+  files.forEach(f => {
+    const sp = f.split('.')
+    const ext = sp[sp.length - 1]
+    if (assets[ext]) {
+      assets[ext].push(f)
+    } else {
+      assets.uncase.push(f)
+    }
+  })
+  return assets
+}
+
 class UploadGithub {
   octokit: any
   pluginName: string
@@ -18,11 +52,6 @@ class UploadGithub {
   repo: string
   outputPath: string
   basePath: string | number
-  assets: {
-    js: string[];
-    css: string[];
-    uncase: string[]
-  }
   constructor(data) {
     const {
       token,
@@ -38,11 +67,6 @@ class UploadGithub {
     this.repo = repo
     this.outputPath = ''
     this.basePath = new Date().getTime()
-    this.assets = {
-      js:[],
-      css:[],
-      uncase:[]
-    }
   }
 
   upload(path, content, message = 'update cdn') {
@@ -58,43 +82,13 @@ class UploadGithub {
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tapAsync(this.pluginName, async (compilation, callback) => {
-      const entrypoints = compilation.entrypoints;
-      const entryNames = Array.from(entrypoints.keys());
-      let files = []
-      for (let i = 0; i < entryNames.length; i++) {
-        const entryName = entryNames[i];
-        const entryFiles = entrypoints.get(entryName).getFiles();
-        files.push(...entryFiles)
-      }
-
-      function unique(arr) {
-        return arr.filter(function (item, index, arr) {
-          return arr.indexOf(item, 0) === index;
-        });
-      }
-
-      files = unique(files)
-      this.assets = {
-        js: [],
-        css: [],
-        uncase: [] // 意料之外的文件
-      }
-
-      files.forEach(f => {
-        const sp = f.split('.')
-        const ext = sp[sp.length - 1]
-        if (this.assets[ext]) {
-          this.assets[ext].push(f)
-        } else {
-          this.assets.uncase.push(f)
-        }
-      })
-      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('UploadGithub', (data, cb) => {
+    compiler.hooks.compilation.tap(this.pluginName, (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('newTest', async (data, cb) => {
+        const assets = getAssets(compilation)
         const cdnUrl = `${this.jsDelivrUrl}/${this.owner}/${this.repo}/${this.basePath}`
         const newAssetJson = ['/']
-        for(let i in this.assets){
-          for(let item of this.assets[i]){
+        for(let i in assets){
+          for(let item of assets[i]){
             data.html = data.html.replace(`/${item}`, `${cdnUrl}/${item}`)
             newAssetJson.push(`${cdnUrl}/${item}`)
           }
@@ -102,15 +96,14 @@ class UploadGithub {
         data.plugin.assetJson = newAssetJson
         cb(null, data)
       })
-      callback();
     })
     compiler.hooks.afterEmit.tapAsync(this.pluginName, async(compilation, callback)=>{
+      const assets = getAssets(compilation)
       const outputPath = compilation.compiler.outputPath;
       const githubUploadList = []
-      for (let i in this.assets) {
-        for (let fileName of this.assets[i]) {
+      for (let i in assets) {
+        for (let fileName of assets[i]) {
           const filePath = `${outputPath}/${fileName}`
-          console.log(filePath)
           const base64 = fileToBase64(filePath)
           const fileUploadPromise = this.upload(`${this.basePath}/${fileName}`, base64).then(res => {
             console.log(`${fileName} upload success`)
